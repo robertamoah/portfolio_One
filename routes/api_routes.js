cea9c7b9dd530c9628e85d64b10991c55ecac96c
Robert Pittman <robertamoah271@gmail.com>
2019-12-19 18:53:42 -0500

    added more css

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,17 +1,5 @@
-var express = require ("express");
-var app = express();
-var exports = module.exports = {};
-
-
-
-module.exports = function robert(){
-
-app.get('/', function (req, res) {
-    res.render('home');
-    // res.send("its go here")
-    res.end()
-     })
-
-
-}
-
+module.exports = function robert() {
+  app.get("/", function(req, res) {
+    console.log("i hit");
+  });
+};
--- server.js
+++ server.js
@@ -4,41 +4,34 @@ const app = express();
 
 const morgan = require("morgan");
 
-var bodyParser = require('body-parser');
+// var bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 
-const port = process.env.Port || 5000;
+const port = process.env.Port || 8080;
 
-const dotenv = require ("dotenv")
-
+const dotenv = require("dotenv");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
-
+  app.use(express.static("client/build"));
+}
 
 // Middlewares
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
+// require("./routes/api_routes")(app);
 
-// dotenv.config()
+dotenv.config();
 //mongo Local connection
-// mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log("connected to DB!")
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB!")
+);
 
-//   )
-
-
-
-
-
-
-
-
-
-app.listen(port, () =>{
-    console.log(`Server Listening at Port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Listening at Port ${port}`);
+});
