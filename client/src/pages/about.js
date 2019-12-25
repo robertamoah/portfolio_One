b0de76915b8e1ed0ca991fd66d167f963e8b26fd
Robert Pittman <robertamoah271@gmail.com>
2019-12-24 21:11:39 -0500

    final

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -9,39 +9,15 @@ class About extends Component {
     return (
       <React.Fragment>
         <h1 className="center1">About</h1>
-        <p>
+        <h2>
           I am a Full Stack Web Developer with experience as a Full Mern
           Application Dev, educated at Passaic County Community College and
           Rutgers Coding Bootcamp. Skills in REACT.js, HTML, CSS, JQuery,
           JavaScript, Git/Github, Passionate about approaching programming
           challenges from different angles and collaborating with others to
           create meaningful web applications.
-        </p>
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-        </p>
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-        </p>
+        </h2>
 
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-        </p>
         <br />
         <br />
         <br />
