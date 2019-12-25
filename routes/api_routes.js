eeaa5e177981692563a9ddce039c1ba275e49464
Robert Pittman <robertamoah271@gmail.com>
2019-12-25 13:30:39 -0500

    hello

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -47,8 +47,4 @@ module.exports = function(app) {
 
     res.send("Email Has been sent, Thank You!");
   });
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 };
