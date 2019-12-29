11a5af199ce0e4d43ff472b6e494265c6c1bd84f
Robert Pittman <robertamoah271@gmail.com>
2019-12-28 20:42:17 -0500

    hello

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const morgan = require("morgan");
 // const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080;
 const dotenv = require("dotenv");
-
+const path = require("path");
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
